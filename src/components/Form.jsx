3b238b7998f3f43c0a27fac66f1a10a27b11054d
Vikash Kumar Gautam <i.vikashgautam07@gmail.com>
2024-08-18 20:27:36 +0530

    original

---
+++ src/components/Form.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Form = () => {
+    return (
+        <div>
+            <h2>Form Component</h2>
+            {/* Add form elements here */}
+        </div>
+    );
+};
+
+export default Form;
---
+++ src/components/ResumePreview.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ResumePreview = () => {
+    return (
+        <div>
+            <h2>Resume Preview Component</h2>
+            {/* Add resume preview elements here */}
+        </div>
+    );
+};
+
+export default ResumePreview;
